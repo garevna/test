a1f06ec2c3bbed9515cc2d8ebc8d7bac496c5063
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-06-02 12:51:31 +0300

    public path removed

--- vue.config.js
+++ vue.config.js
@@ -17,15 +17,12 @@ module.exports = {
     themeColor: '#20731C',
     msTileColor: '#FAFAFA',
     iconPaths: {
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon.png',
-      maskIcon: 'img/icons/favicon.png',
-      msTileImage: 'img/icons/mstile-150x150.png'
+      favicon32: '/img/icons/favicon-32x32.png',
+      favicon16: '/img/icons/favicon-16x16.png',
+      appleTouchIcon: '/img/icons/apple-touch-icon.png',
+      maskIcon: '/img/icons/favicon.png',
+      msTileImage: '/img/icons/mstile-150x150.png'
     }
   },
-  runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/connect-melbournecbd/'
-    : '/'
+  runtimeCompiler: true
 }
