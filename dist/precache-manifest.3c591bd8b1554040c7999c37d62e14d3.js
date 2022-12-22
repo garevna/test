bd12f484c59068d3332fc46d770480c348910eaf
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-12-22 11:21:13 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.3c591bd8b1554040c7999c37d62e14d3.js
@@ -0,0 +1,526 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bec297eeb4cf05165238dbe46f7e54",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "6163a5ac6ffb5495db326327f669dfef",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "5fe74419c0081b34e1ae116a7976a426",
+    "url": "/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "566c8de8bbf68dc6e6c21b527ff941a8",
+    "url": "/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "2ff4e49987b148c3b151a000feaf1a44",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "77a3bf6dc13b355a7d5b",
+    "url": "/css/app.06bda4bb.css"
+  },
+  {
+    "revision": "1ae5b91b0cd1a0218940",
+    "url": "/css/blackfriday.a5ae6596.css"
+  },
+  {
+    "revision": "177b1884c3fef2a06005",
+    "url": "/css/blackfriday.home.not-found.page.7246f01c.css"
+  },
+  {
+    "revision": "f6142c7b8147e061744d",
+    "url": "/css/chunk-vendors.29dbbaea.css"
+  },
+  {
+    "revision": "bdc99470834816cc60e7",
+    "url": "/css/faqs.170d32b0.css"
+  },
+  {
+    "revision": "2e30aa54e3ed4eea0155",
+    "url": "/css/footer-component.315aa0a1.css"
+  },
+  {
+    "revision": "58ba0af1730970f93a65",
+    "url": "/css/home.f1ee91e4.css"
+  },
+  {
+    "revision": "020ac7a09e47a2543086",
+    "url": "/css/main-menu.f091c936.css"
+  },
+  {
+    "revision": "9c74112d74e05e12fdfc",
+    "url": "/css/not-found.11c29232.css"
+  },
+  {
+    "revision": "2afb5b8248b4380c6e43",
+    "url": "/css/page.73565f75.css"
+  },
+  {
+    "revision": "7fd3f226e4a50fcd9f93",
+    "url": "/css/reviews.49d0453c.css"
+  },
+  {
+    "revision": "70b8086d6dba6128cdcf",
+    "url": "/css/userForm.6d87831d.css"
+  },
+  {
+    "revision": "3ee088ebf456531c3c79586af139b62f",
+    "url": "/fonts/Gilroy-Black.eot"
+  },
+  {
+    "revision": "d59719bcf2c0c2e0db325ecf56c1d257",
+    "url": "/fonts/Gilroy-Black.ttf"
+  },
+  {
+    "revision": "bde8bcef5590fd0ee6bca94690ceb069",
+    "url": "/fonts/Gilroy-Black.woff"
+  },
+  {
+    "revision": "df2b7fe02426d3238da8595b41808c3e",
+    "url": "/fonts/Gilroy-BlackItalic.eot"
+  },
+  {
+    "revision": "25a0c193cf573450c069971145972e7c",
+    "url": "/fonts/Gilroy-BlackItalic.ttf"
+  },
+  {
+    "revision": "7fc70f6ca1a2c909af0889d2c5c01e18",
+    "url": "/fonts/Gilroy-BlackItalic.woff"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/fonts/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/fonts/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/fonts/Gilroy-Bold.5e111ca0.eot"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/fonts/Gilroy-Bold.eot"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/fonts/Gilroy-Bold.ttf"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/fonts/Gilroy-Bold.woff"
+  },
+  {
+    "revision": "ebdd31004ad2b93d72aba2703ec51cbb",
+    "url": "/fonts/Gilroy-BoldItalic.eot"
+  },
+  {
+    "revision": "2b56aed03785343b8a9d2ab464f35d61",
+    "url": "/fonts/Gilroy-BoldItalic.ttf"
+  },
+  {
+    "revision": "6171e2e979cc8f36f66f88785aedd506",
+    "url": "/fonts/Gilroy-BoldItalic.woff"
+  },
+  {
+    "revision": "bdf3ea41ca5484b3f973d14a46cad4a4",
+    "url": "/fonts/Gilroy-ExtraBold.eot"
+  },
+  {
+    "revision": "07e6a6cd3cdb9eedaa9aa81fafe1e42a",
+    "url": "/fonts/Gilroy-ExtraBold.ttf"
+  },
+  {
+    "revision": "2037b7c09569dd6f0ca302d92ec0c23e",
+    "url": "/fonts/Gilroy-ExtraBold.woff"
+  },
+  {
+    "revision": "04a0adfb6c10582159d8bc13a66550ac",
+    "url": "/fonts/Gilroy-ExtraBoldItalic.eot"
+  },
+  {
+    "revision": "1ccd0d2f9c282cf2e80d0b455164cd69",
+    "url": "/fonts/Gilroy-ExtraBoldItalic.ttf"
+  },
+  {
+    "revision": "e43cca6cb170481eb7dfb7674c525368",
+    "url": "/fonts/Gilroy-ExtraBoldItalic.woff"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/fonts/Gilroy-Heavy.335c7ebf.eot"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/fonts/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/fonts/Gilroy-Heavy.eot"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/fonts/Gilroy-Heavy.ffa5800e.woff"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/fonts/Gilroy-Heavy.ttf"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/fonts/Gilroy-Heavy.woff"
+  },
+  {
+    "revision": "de6650409109030f3eecf80059165849",
+    "url": "/fonts/Gilroy-HeavyItalic.eot"
+  },
+  {
+    "revision": "574db652a041efb7bb8e55c280c797ec",
+    "url": "/fonts/Gilroy-HeavyItalic.ttf"
+  },
+  {
+    "revision": "c9a8c7bb42cb32a97db04a2be4b397d3",
+    "url": "/fonts/Gilroy-HeavyItalic.woff"
+  },
+  {
+    "revision": "c2a5327dd2426ad4cbdd4868010d88dd",
+    "url": "/fonts/Gilroy-Light.eot"
+  },
+  {
+    "revision": "73bfa12b55452a65a0253c511856d6c2",
+    "url": "/fonts/Gilroy-Light.ttf"
+  },
+  {
+    "revision": "f9b62dd8f7c5cc5820eee7743544d321",
+    "url": "/fonts/Gilroy-Light.woff"
+  },
+  {
+    "revision": "b276abdd786e0265d78e817c26859a28",
+    "url": "/fonts/Gilroy-LightItalic.eot"
+  },
+  {
+    "revision": "a774850a6f3bebd595224064172b5eba",
+    "url": "/fonts/Gilroy-LightItalic.ttf"
+  },
+  {
+    "revision": "0cf6584c45aa11ddc60d113d1c0cb12d",
+    "url": "/fonts/Gilroy-LightItalic.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/fonts/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/fonts/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/fonts/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/fonts/Gilroy-Medium.eot"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/fonts/Gilroy-Medium.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/fonts/Gilroy-Medium.woff"
+  },
+  {
+    "revision": "27cc53307e05098f42c65b79d8469c92",
+    "url": "/fonts/Gilroy-MediumItalic.eot"
+  },
+  {
+    "revision": "9bc60a85e3c00995d5e2878f00f9a36e",
+    "url": "/fonts/Gilroy-MediumItalic.ttf"
+  },
+  {
+    "revision": "f8ac8bc5930ab416a4029a7b0a563549",
+    "url": "/fonts/Gilroy-MediumItalic.woff"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/fonts/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/fonts/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/fonts/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/fonts/Gilroy-Regular.eot"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/fonts/Gilroy-Regular.ttf"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/fonts/Gilroy-Regular.woff"
+  },
+  {
+    "revision": "d2d1a69ff6039f84d651397d818e97a8",
+    "url": "/fonts/Gilroy-RegularItalic.eot"
+  },
+  {
+    "revision": "4b9bfe4dcf729744ecbf9415d002abb2",
+    "url": "/fonts/Gilroy-RegularItalic.ttf"
+  },
+  {
+    "revision": "5b729efbf680111713791feb93704c19",
+    "url": "/fonts/Gilroy-RegularItalic.woff"
+  },
+  {
+    "revision": "2b2e536ee22048c9e56aebfc0fb6298d",
+    "url": "/fonts/Gilroy-SemiBold.eot"
+  },
+  {
+    "revision": "05bdf30b8aaa10683c19e73dd0c428da",
+    "url": "/fonts/Gilroy-SemiBold.ttf"
+  },
+  {
+    "revision": "989947b092774c1d2811236856bbdcc3",
+    "url": "/fonts/Gilroy-SemiBold.woff"
+  },
+  {
+    "revision": "eebe073afc380ddff4cb1118d84274ba",
+    "url": "/fonts/Gilroy-SemiBoldItalic.eot"
+  },
+  {
+    "revision": "d9652658411a4e21bfaab0b60df39596",
+    "url": "/fonts/Gilroy-SemiBoldItalic.ttf"
+  },
+  {
+    "revision": "2706d075b331aadf820ab7044b3555de",
+    "url": "/fonts/Gilroy-SemiBoldItalic.woff"
+  },
+  {
+    "revision": "936acdcb3f6bd61bb534f20d3393738a",
+    "url": "/fonts/Gilroy-Thin.eot"
+  },
+  {
+    "revision": "b4e27db9f692dbeec0d2a59cd94194b3",
+    "url": "/fonts/Gilroy-Thin.ttf"
+  },
+  {
+    "revision": "e1358f50dab25bfa576ec5b363961e96",
+    "url": "/fonts/Gilroy-Thin.woff"
+  },
+  {
+    "revision": "0af99171790a53c29687efd52cbefcb9",
+    "url": "/fonts/Gilroy-ThinItalic.eot"
+  },
+  {
+    "revision": "9575e89230cc2f26e6e95436175f63cc",
+    "url": "/fonts/Gilroy-ThinItalic.ttf"
+  },
+  {
+    "revision": "72bca972ab621c1940f7ca0324da7f9b",
+    "url": "/fonts/Gilroy-ThinItalic.woff"
+  },
+  {
+    "revision": "1b3e628e784f8ade6bc725a5a6702267",
+    "url": "/fonts/Gilroy-UltraLight.eot"
+  },
+  {
+    "revision": "d3d30383ec85ad32cc9b7ac440646875",
+    "url": "/fonts/Gilroy-UltraLight.ttf"
+  },
+  {
+    "revision": "22b0d1f59b420e6c71deb64ac83797e9",
+    "url": "/fonts/Gilroy-UltraLight.woff"
+  },
+  {
+    "revision": "0bf003f535a1876813dc8fc7ef985c64",
+    "url": "/fonts/Gilroy-UltraLightItalic.eot"
+  },
+  {
+    "revision": "21f2f1d85399276d2ef78ebc79cd253c",
+    "url": "/fonts/Gilroy-UltraLightItalic.ttf"
+  },
+  {
+    "revision": "f07d7e71351c683a2f450e97fe00e693",
+    "url": "/fonts/Gilroy-UltraLightItalic.woff"
+  },
+  {
+    "revision": "3f7f84c156d7fc89b83905fe8f2a2f8d",
+    "url": "/fonts/manifest.json"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "0f6246a959f5e411fc59e31a1ddc3010",
+    "url": "/img/dgtek-logo-white.0f6246a9.svg"
+  },
+  {
+    "revision": "992dc132a88db5cbc2c9c37b07e185d8",
+    "url": "/img/dgtek-logo.992dc132.svg"
+  },
+  {
+    "revision": "ada5061dc20107863af0d42cbf567dda",
+    "url": "/img/dgtek-logo.png"
+  },
+  {
+    "revision": "64427e15ac5261f4a52128d53350bc94",
+    "url": "/img/testimonials/1.jpg"
+  },
+  {
+    "revision": "b12f1b44479fe1aa0a45e59394180d63",
+    "url": "/img/testimonials/10.png"
+  },
+  {
+    "revision": "5e31a29026b1e8afa447ee141802ff73",
+    "url": "/img/testimonials/11.png"
+  },
+  {
+    "revision": "50cd42dfecce77f1862fe03dfe99f4ff",
+    "url": "/img/testimonials/12.png"
+  },
+  {
+    "revision": "bde2502306971300e34b8d39ad8adca3",
+    "url": "/img/testimonials/13.png"
+  },
+  {
+    "revision": "7b5a4655e9a28a9bca66df0032e7ba28",
+    "url": "/img/testimonials/14.png"
+  },
+  {
+    "revision": "76c49b31dde860cc48a06e785c9aed1e",
+    "url": "/img/testimonials/15.png"
+  },
+  {
+    "revision": "a7055035c5a5ac52b3705e1c650cbc43",
+    "url": "/img/testimonials/2.jpg"
+  },
+  {
+    "revision": "e37a57d692b876c600a673e561b05c6d",
+    "url": "/img/testimonials/3.jpg"
+  },
+  {
+    "revision": "0327ed81812baf95bbda07b71b81a12e",
+    "url": "/img/testimonials/4.png"
+  },
+  {
+    "revision": "ca51aba41cbcf0e31ab7f55c2625af74",
+    "url": "/img/testimonials/5.png"
+  },
+  {
+    "revision": "f634a0bacf523e74cab3e67419602783",
+    "url": "/img/testimonials/6.png"
+  },
+  {
+    "revision": "867d82adf3b6c09b5f9d1b3246f1c814",
+    "url": "/img/testimonials/7.png"
+  },
+  {
+    "revision": "bb68f71d86ff786d01e710fa628eb44c",
+    "url": "/img/testimonials/8.png"
+  },
+  {
+    "revision": "38da7e779b5d3d6642b479f4bd3a15e7",
+    "url": "/img/testimonials/9.png"
+  },
+  {
+    "revision": "64f3a7dd82f399db86d0911b3fe9c057",
+    "url": "/img/testimonials/default.png"
+  },
+  {
+    "revision": "61dafc9c31e2756a211c15f482c6fe22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a3bf6dc13b355a7d5b",
+    "url": "/js/app.ae4a9bd5.js"
+  },
+  {
+    "revision": "1ae5b91b0cd1a0218940",
+    "url": "/js/blackfriday.828c1a5f.js"
+  },
+  {
+    "revision": "177b1884c3fef2a06005",
+    "url": "/js/blackfriday.home.not-found.page.aba732bf.js"
+  },
+  {
+    "revision": "f6142c7b8147e061744d",
+    "url": "/js/chunk-vendors.292674f4.js"
+  },
+  {
+    "revision": "bdc99470834816cc60e7",
+    "url": "/js/faqs.4f11e69f.js"
+  },
+  {
+    "revision": "2e30aa54e3ed4eea0155",
+    "url": "/js/footer-component.2eb6d522.js"
+  },
+  {
+    "revision": "58ba0af1730970f93a65",
+    "url": "/js/home.f3e92043.js"
+  },
+  {
+    "revision": "020ac7a09e47a2543086",
+    "url": "/js/main-menu.aa3e8beb.js"
+  },
+  {
+    "revision": "9c74112d74e05e12fdfc",
+    "url": "/js/not-found.89f77946.js"
+  },
+  {
+    "revision": "2afb5b8248b4380c6e43",
+    "url": "/js/page.69d11f3e.js"
+  },
+  {
+    "revision": "7fd3f226e4a50fcd9f93",
+    "url": "/js/reviews.74a160e6.js"
+  },
+  {
+    "revision": "70b8086d6dba6128cdcf",
+    "url": "/js/userForm.edbdd641.js"
+  },
+  {
+    "revision": "41d719318fb9fb2901aab8951684759e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b5107077fa45b30c3b227f7079df15c3",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "dc50fcbebdcb049a1c622758fd8eab82",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8633a26c079544a29df27aa1bb8b885b",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "f551c5cd06ff6c5b92d140b5e1db5ca2",
+    "url": "/testimonials/testimonials.json"
+  }
+]);
