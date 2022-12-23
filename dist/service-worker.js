efff7d4a2a1de8bccae8d190a8fb561cd9d7f3ff
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-12-23 13:44:58 +0200

    13:45: dist to deploy on branch gh-pages

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,24 +11,43 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+/* eslint-disable */
 
-importScripts(
-  "/test/precache-manifest.1e2b0f8a234aaae20f8bc02cf1ab1be6.js"
-);
+const CACHE_NAME = `live.pineapple.net.au-${(new Date()).toISOString().slice(0, 10)}`
 
-workbox.core.setCacheNameDetails({prefix: "live.pineapple.net.au"});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+importScripts('/test/precache-manifest.1e2b0f8a234aaae20f8bc02cf1ab1be6.js')
+
+workbox.core.setCacheNameDetails({ prefix: 'live.pineapple.net.au' })
 
 self.addEventListener('message', (event) => {
+  console.log('SERVICE WORKER MESSAGE EVENT:\n', event)
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
+
+self.addEventListener('activate', function (event) {
+  console.log('ACTIVATE\n', event)
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          console.log('CASH NAME: ', cacheName)
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('live.pineapple.net.au')) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
