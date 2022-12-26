2fd242d8e9f06fa3f611d6b94fd153801482b137
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-12-26 06:04:16 +0200

    Service worker test 2022-12-26 at 06:05

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,11 +11,36 @@
  * See https://goo.gl/2aRDsh
  */
 
+import { cacheNames } from 'workbox-core'
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 importScripts('/test/precache-manifest.970442ff271905a98550f6a06238deba.js')
 
-workbox.core.setCacheNameDetails({ prefix: 'live.pineapple.net.au' })
+console.log('CACHE NAMES BEFORE:\n', cacheNames)
+console.log(cacheNames.precache)
+console.log(cacheNames.runtime)
+console.log(cacheNames.googleAnalytics)
+
+// var CACHE_NAME = 'live-pineapple-cache'
+
+workbox.core.setCacheNameDetails({
+  prefix: 'live-pineapple',
+  suffix: 'v1',
+  precache: 'install-time',
+  runtime: 'run-time',
+  googleAnalytics: 'ga'
+})
+
+console.log('CACHE NAMES AFTER:\n', cacheNames)
+
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       return cache.addAll(CACHED_URLS)
+//     })
+//   )
+// })
 
 self.addEventListener('message', (event) => {
   console.log('SERVICE WORKER MESSAGE EVENT:\n', event)
