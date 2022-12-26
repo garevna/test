00659350c927a5672c3b73b28b94204866b1e23a
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2022-12-26 06:16:06 +0200

    Service worker test 2022-12-26 at 06:16

--- service-worker.js
+++ service-worker.js
@@ -11,18 +11,18 @@
  * See https://goo.gl/2aRDsh
  */
 
-import { cacheNames } from 'workbox-core'
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 importScripts('/test/precache-manifest.970442ff271905a98550f6a06238deba.js')
 
-console.log('CACHE NAMES BEFORE:\n', cacheNames)
-console.log(cacheNames.precache)
-console.log(cacheNames.runtime)
-console.log(cacheNames.googleAnalytics)
+// console.log('CACHE NAMES BEFORE:\n', cacheNames)
+// console.log(cacheNames.precache)
+// console.log(cacheNames.runtime)
+// console.log(cacheNames.googleAnalytics)
 
 // var CACHE_NAME = 'live-pineapple-cache'
+
+console.log('WORK BOX:\n', workbox)
 
 workbox.core.setCacheNameDetails({
   prefix: 'live-pineapple',
@@ -32,7 +32,7 @@ workbox.core.setCacheNameDetails({
   googleAnalytics: 'ga'
 })
 
-console.log('CACHE NAMES AFTER:\n', cacheNames)
+// console.log('CACHE NAMES AFTER:\n', cacheNames)
 
 // self.addEventListener('install', function(event) {
 //   event.waitUntil(
