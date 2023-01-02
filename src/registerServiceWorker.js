f5d743aab2c65c213fdc682d2e9b1d9266ca629d
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2023-01-02 20:15:42 +0200

    Notification to reload

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,20 +2,20 @@
 
 import { register } from 'register-service-worker'
 
-import { serviceWorkerConfig } from './helpers'
+import { /* serviceWorkerConfig, */ notification } from './helpers'
 
-function showWorkers (title, registration) {
-  console.group(title)
-  const { active, waiting } = registration
-  if (title === 'REGISTERED') console.log('REGISTRATION OBJECT:\n', registration)
-  console.table([
-    { name: active.name, state: active.state },
-    { name: waiting ? waiting.name : null, state: waiting ? waiting.state : null }
-  ])
-  // console.log('ACTIVE SERVICE WORKER:\n', registration.active)
-  // console.log('WAITING SERVICE WORKER:\n', registration.waiting)
-  console.groupEnd(title)
-}
+// function showWorkers (title, registration) {
+//   console.group(title)
+//   const { active, waiting } = registration
+//   if (title === 'REGISTERED') console.log('REGISTRATION OBJECT:\n', registration)
+//   console.table([
+//     { name: active.name, state: active.state },
+//     { name: waiting ? waiting.name : null, state: waiting ? waiting.state : null }
+//   ])
+//   // console.log('ACTIVE SERVICE WORKER:\n', registration.active)
+//   // console.log('WAITING SERVICE WORKER:\n', registration.waiting)
+//   console.groupEnd(title)
+// }
 
 register(`${process.env.BASE_URL}service-worker.js`, {
   ready (registration) {
@@ -38,12 +38,14 @@ register(`${process.env.BASE_URL}service-worker.js`, {
   updated (registration) {
     console.log('New content is available.')
 
-    Object.assign(registration.active, { name: 'Active service worker', [`${registration.active.state}`]: new Date().toLocaleString() })
-    Object.assign(registration.waiting, { name: 'Waiting service worker', [`${registration.waiting.state}`]: new Date().toLocaleString() })
+    notification()
 
-    showWorkers('UPDATED', registration)
+    // Object.assign(registration.active, { name: 'Active service worker', [`${registration.active.state}`]: new Date().toLocaleString() })
+    // Object.assign(registration.waiting, { name: 'Waiting service worker', [`${registration.waiting.state}`]: new Date().toLocaleString() })
 
-    serviceWorkerConfig()
+    // showWorkers('UPDATED', registration)
+
+    // serviceWorkerConfig()
   },
 
   offline () {
