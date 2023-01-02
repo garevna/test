0525b47d104b7c5842898320e5eb6b37b9f12400
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2023-01-02 15:04:20 +0200

    Test-normal-SW-2023-01-02-15:04

--- service-worker.js
+++ service-worker.js
@@ -11,47 +11,24 @@
  * See https://goo.gl/2aRDsh
  */
 
-/* eslint-disable no-undef */
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts(
+  "/test/precache-manifest.452a39bcdf2c441d8498a82331506f2f.js"
+);
 
-importScripts('/test/precache-manifest.15675878bb7535652a393bbd328cc73a.js')
-
-console.log(workbox)
-
-workbox.core.setCacheNameDetails({ prefix: 'live.pineapple.net.au' })
-
-self.addEventListener('fetch', fetchEvent => {
-  console.log('FETCH EVENT:\n', fetchEvent)
-  console.log('fetching', fetchEvent.request)
-  console.log('CACHES:\n', caches)
-  fetchEvent.respondWith(caches.match(fetchEvent.request)
-    .then(async cachedResponse => {
-      console.log('CACHED RESPONSE:\n', cachedResponse)
-      if (cachedResponse) return cachedResponse
-      const response = await fetch(fetchEvent.request)
-      caches.open('whatever')
-        .then(cache => cache.put(fetchEvent.request, response))
-      return response.clone()
-    })
-  )
-})
+workbox.core.setCacheNameDetails({prefix: "live.pineapple.net.au"});
 
 self.addEventListener('message', (event) => {
-  console.log('SERVICE WORKER MESSAGE EVENT:\n', event)
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
+    self.skipWaiting();
   }
-})
-
-self.addEventListener('push', () => event.waitUntil(fetch('/updates').then(() => self.registration.showNotification('New updates'))))
-
-self.postMessage('SERVICE WORKER HERE!!!')
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
